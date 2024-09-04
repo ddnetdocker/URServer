aedf3aa12562af905244e8cdf7079bd2f93ba3a9
Daniel Kempf <daniel.kempf@fluidon.com>
2024-09-04 20:09:08 +0200

    default init of mod.h

--- src/game/server/gamemodes/mod.h
+++ src/game/server/gamemodes/mod.h
@@ -9,6 +9,18 @@ public:
 	CGameControllerMod(class CGameContext *pGameServer);
 	~CGameControllerMod();
 
+	CScore *Score();
+
+	void HandleCharacterTiles(class CCharacter *pChr, int MapIndex) override;
+	void SetArmorProgress(CCharacter *pCharacer, int Progress) override;
+
+	void OnPlayerConnect(class CPlayer *pPlayer) override;
+	void OnPlayerDisconnect(class CPlayer *pPlayer, const char *pReason) override;
+
+	void OnReset() override;
+
 	void Tick() override;
+
+	void DoTeamChange(class CPlayer *pPlayer, int Team, bool DoChatMsg = true) override;
 };
 #endif // GAME_SERVER_GAMEMODES_MOD_H
